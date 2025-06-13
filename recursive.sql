60b81883577302e05890736774c5e91436fb027a
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2025-06-13 01:30:07 -0400

    Improved comments

--- recursive.sql
+++ recursive.sql
@@ -1,15 +1,15 @@
 --
--- We *should* be able to write this type of hierarchy query in SQL.
+-- We SHOULD be able to write this type of hierarchy query in SQL.
 --
 -- A robust solution for hierarchical queries in SQL requires recursive SQL.
 -- Not supported in the original SQL standard but was added in SQL:1999.
 --
--- BTW, this type of query is often referred to by other names:
+-- This type of query is often referred to by other names:
 --    * Transitive closure
 --    * Bill of materials problem 
 --    * Parts explosion problem
 --
--- 
+--
 with recursive rl(empno, ename, job, mgr) as
 (select empno, ename, job, mgr from emp where ename = 'ADAMS'
     union all
@@ -17,25 +17,20 @@ with recursive rl(empno, ename, job, mgr) as
 select * from rl;
 -- 
 --  Recursive CTE syntax and structure
---    requires keyword 
---    recursive CTE is always a union or union all of two queries
+--    requires recursive keyword 
+--    recursive CTE body is always a union or union all of two queries
 --    1st query (non-recursive select): 
---        * evaluated 1st 
---        * must be a non-recursive select that queries the hierarchy table
+--        * evaluated 1st and only once 
+--        * select that queries the hierarchy table
 --    2nd query (recursive select): 
 --        * evaluated 2nd - Nth times 
 --        * join of the CTE to the hierarchy table
 --
--- Recursive CTE Evaluation
---
---   
---   1. Execute non-recursive select
---   2. Add results to QR and WT
---   3. Repeat until WT is empty
---       a. Execute <recursive select> using WT data for <cte-name>
---       b. Add results to QR
---       c. Replace WT with results
-
-
-
-
+--  Recursive CTE Evaluation   
+--    1. Execute non-recursive select
+--    2. Add results to QR and WT
+--    3. Repeat until WT is empty
+--        a. Execute <recursive select> using WT data for <cte-name>
+--        b. Add results to QR
+--        c. Replace WT with results
+--    4. Return QR
